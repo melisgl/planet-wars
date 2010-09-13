9ff670e7c4edb1d5374c2e7385927eb17bfdbb5f
Gábor Melis <mega@retes.hu>
2010-09-13 22:00:53 +0200

    compiles

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ BIN=my-bot
 
 $BIN: src/*.lisp
 	sh run-this.sh --eval \
-        "(save-lisp-and-die \"$BIN\" :executable t :toplevel #'planet-wars:main)"
+        "(save-lisp-and-die \"$(BIN)\" :executable t :toplevel #'planet-wars:main)"
 
 clean:
 	rm -f src/*.fasl src/*~
 
 distclean: clean
-	rm -f "$BIN" "$DIST" "$SUBMISSION"
+	rm -f "$(BIN)" "$(DIST)" "$(SUBMISSION)"
 
 dist:
 	git archive --format=zip --prefix "bocsimacko/" HEAD > $(DIST)
---
+++ MyBot.lisp
@@ -0,0 +1,19 @@
+;;;; This file must be here because the contest server compile
+;;;; environment relies on it.
+;;;;
+;;;; ASDF on the server gets into infinite recursion so load stuff
+;;;; manually.
+
+(defparameter *interesting-debug-info* nil)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun compile-and-load (interesting-debug-info)
+    (declare (optimize debug))
+    (setq *interesting-debug-info* interesting-debug-info)
+    (dolist (name '("parse-number" "split-sequence"
+                    "package" "model" "io" "play"))
+      (load (compile-file (format nil "src/~A.lisp" name))))))
+
+(compile-and-load (list *default-pathname-defaults*
+                        *features*
+                        (directory "**")))
