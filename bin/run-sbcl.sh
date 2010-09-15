58a53a3ce530c53f48599c90aba3f7ebc3976f69
Gábor Melis <mega@retes.hu>
2010-09-15 15:47:55 +0200

    heap

--- bin/run-sbcl.sh
+++ bin/run-sbcl.sh
@@ -6,7 +6,7 @@
 ASDF_DIRS_FILE=`dirname "$0"`/../.asdf-dirs
 ASDF_DIRS=`cat "$ASDF_DIRS_FILE"`
 
-sbcl --dynamic-space-size 896 --noinform --lose-on-corruption \
+sbcl --dynamic-space-size 768 --noinform --lose-on-corruption \
     --end-runtime-options \
     --noprint --no-userinit --no-sysinit --disable-debugger \
     --eval "(setf asdf:*central-registry* (list $ASDF_DIRS))" \
