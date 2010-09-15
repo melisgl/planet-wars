125716c3331b536e6237d1e35922e689c758c2ab
Gábor Melis <mega@retes.hu>
2010-09-15 15:43:40 +0200

    comments

--- bin/run-bot.sh
+++ bin/run-bot.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+#
+# Pass the path to this script to an engine (e.g. PlayGame.jar) as the
+# player argument. It loads the planet-wars system and calls PW:PLAY.
 BASE=`dirname "$0"`
 
 "$BASE"/run-sbcl.sh --load "$BASE/../MyBot.lisp"
--- bin/run-sbcl.sh
+++ bin/run-sbcl.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+#
+# Wrapper for sbcl. Set common command line arguments to sbcl and
+# initialize ASDF:*CENTRAL-REGISTRY*.
+
 ASDF_DIRS_FILE=`dirname "$0"`/../.asdf-dirs
 ASDF_DIRS=`cat "$ASDF_DIRS_FILE"`
 
-sbcl --dynamic-space-size 768 --noinform --lose-on-corruption \
+sbcl --dynamic-space-size 896 --noinform --lose-on-corruption \
     --end-runtime-options \
     --noprint --no-userinit --no-sysinit --disable-debugger \
     --eval "(setf asdf:*central-registry* (list $ASDF_DIRS))" \
